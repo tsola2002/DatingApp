3c2c4abd7e0530be5c9c9b39401b39e7a069dadb
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-11-22 06:22:08 +0100

    Adding Routing to our nav component links

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,28 +1,41 @@
+// modules that come from node modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
+// locally created modules
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      // we tell routermodule about the routes we specified
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+
+// routes is an array with each route as an object
+// we provide a path and match it to a component
+// the last componet is a wildcard component
+// angular router operates on a first line wins system
+export const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'members', component: MemberListComponent },
+    { path: 'messages', component: MessagesComponent },
+    { path: 'lists', component: ListsComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
