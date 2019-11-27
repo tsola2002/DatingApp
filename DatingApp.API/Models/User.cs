bb1ae63be3754238b7b547510ff10bc804ed2266
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-11-27 06:31:16 +0100

    updating mymigrations

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -6,22 +6,6 @@ namespace DatingApp.API.Models
 {
     public class User
     {
-        public User(int id, string username, string gender, DateTime dateOfBirth, string knownAs, DateTime created, DateTime lastActive, string introduction, string lookingFor, string interests, string city, string country)
-        {
-            this.Id = id;
-            this.Username = username;
-            this.Gender = gender;
-            this.DateOfBirth = dateOfBirth;
-            this.KnownAs = knownAs;
-            this.Created = created;
-            this.LastActive = lastActive;
-            this.Introduction = introduction;
-            this.LookingFor = lookingFor;
-            this.Interests = interests;
-            this.City = city;
-            this.Country = country;
-
-        }
         public int Id { get; set; }
 
         public string Username { get; set; }
