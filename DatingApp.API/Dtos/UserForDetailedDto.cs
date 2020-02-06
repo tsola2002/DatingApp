7a9cf7330a60f0ac54d9cd5cd2f423507a209179
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-02-06 06:22:44 +0100

    shaping the data with Data Transfer Objects

---
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForDetailedDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Gender { get; set; }
+        // DateTime type will represent date of birth(you alos have to import system for it) 
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        // to help show long a user has been a member
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
---
+++ DatingApp.API/Dtos/UserForListDto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForListDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+
+        public string Gender { get; set; }
+        //DateTime type will represent date of birth(you alos have to import system for it) 
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        // to help show long a user has been a member
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
