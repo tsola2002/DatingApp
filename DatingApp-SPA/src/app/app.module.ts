693f8e6c2ca4a0fdd76d623da01f74096807dd91
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-02-09 16:20:47 +0100

    adding a tabbed panel for the member-detailed page

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -43,6 +43,7 @@ export function tokenGetter() {
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
       // we tell routermodule about the routes we specified
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -10,14 +10,16 @@ namespace DatingApp.API.Dtos
         public int Id { get; set; }
 
         public string Username { get; set; }
-
         public string Gender { get; set; }
-        // DateTime type will represent date of birth(you alos have to import system for it) 
+        //DateTime type will represent date of birth(you alos have to import system for it) 
         public int Age { get; set; }
         public string KnownAs { get; set; }
         // to help show long a user has been a member
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
